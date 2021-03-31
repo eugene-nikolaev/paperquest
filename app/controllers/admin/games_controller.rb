b5c1425dc8c8693606bd6f215e3392081312cf5d
Eugene Nikolaev <eug.nikolaev@gmail.com>
2021-03-31 20:07:46 +0300

    Implement saving game

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -3,7 +3,30 @@ class Admin::GamesController < ApplicationController
    
   end
 
-  def save
+  def create
+    game_attrs = params[:game] || {}
+    name = game_attrs[:name] || ""
+    questions_text = game_attrs[:questions] || ""
+    questions = questions_text.split(/(\r\n){2,}/).reject { |it| it.strip.empty? }
+    if questions.empty?
+      flash[:error] = "No questions"
+    else
+      Game.transaction do
+        game = Game.new(
+          pincode: rand(1_000_000_000..9_999_999_999),\
+          name:    name
+        )
+        game.save!
+        questions.each_with_index do |question, index|
+          task = Task.new(
+            game_id: game.id,
+            position: index + 1,
+            question: question
+          )
+          task.save!
+        end
+      end
+    end
   end
 
   def edit
